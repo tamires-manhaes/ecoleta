0adbd9f8149aad5e4f96434bd3ea056a313bc47a
Tamires <tamiresmanhaes4@gmail.com>
2020-06-06 22:31:16 -0300

    Backend finalizado funcionando

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+const PORT = 3333;
+app.listen(PORT);
+
+
